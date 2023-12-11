d81f03388a681c8a908cca10b084d43315536634
Simonas Mulevicius <50625590+simonasmulevicius@users.noreply.github.com>
2023-12-11 22:21:50 +0200

    feat: adding a sample diagram

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from dash import Dash, html, dash_table
+from dash import Dash, html, dash_table, dcc
 import pandas as pd
+import plotly.express as px
 
 df = pd.read_csv(
     "https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv"
@@ -11,6 +12,7 @@ app.layout = html.Div(
     [
         html.Div(children="Kuris seimo narys mane atstovauja geriausiai?"),
         dash_table.DataTable(data=df.to_dict("records"), page_size=10),
+        dcc.Graph(figure=px.histogram(df, x="continent", y="lifeExp", histfunc="avg")),
     ]
 )
 
