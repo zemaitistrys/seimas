3da73fd57ea56f63a23c282a8fd9ed07e785adcc
Simonas Mulevicius <50625590+simonasmulevicius@users.noreply.github.com>
2023-12-11 22:17:57 +0200

    feat: using loaded data

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-from dash import Dash, html
+from dash import Dash, html, dash_table
+import pandas as pd
+
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv"
+)
 
 app = Dash(__name__)
 
-app.layout = html.Div([html.Div(children="Hello World")])
+app.layout = html.Div(
+    [
+        html.Div(children="Kuris seimo narys mane atstovauja geriausiai?"),
+        dash_table.DataTable(data=df.to_dict("records"), page_size=10),
+    ]
+)
 
 if __name__ == "__main__":
     app.run(debug=True)
