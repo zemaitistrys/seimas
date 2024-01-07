fc9a67883c5e7ded05b092fa566bf36062e570b0
Simonas Mulevicius <50625590+simonasmulevicius@users.noreply.github.com>
2024-01-07 18:03:52 +0200

    feat: adding dynamic calculation for results

--- app.py
+++ app.py
@@ -1,18 +1,42 @@
-from dash import Dash, html, dcc, callback, callback_context, Output, Input, MATCH, ALL
+from dash import (
+    Dash,
+    html,
+    dcc,
+    callback,
+    callback_context,
+    Output,
+    Input,
+    MATCH,
+    State,
+    ALL,
+)
 import dash_bootstrap_components as dbc
 import json
+from enum import Enum
+from constants import question_contents
 
 app_name = "RenkuProtingai.lt"
 app = Dash(app_name, external_stylesheets=[dbc.themes.LUX])
 app.title = app_name
 server = app.server
 
+
+class AnswerOptions(Enum):
+    FOR = "for"
+    AGAINST = "against"
+    DONTCARE = "dontcare"
+
+
 html.Div([html.Link(rel="icon", href="/assets/favicon.ico")])
+
 
 question_elements = [
     html.Div(
         [
-            html.H6(f"Klausimas {question_id}", className="mt-3"),
+            html.H6(
+                f"{question_id+1}. {question['motion_title_for_questionaire']}",
+                className="mt-3",
+            ),
             dbc.Row(
                 [
                     dbc.Col(
@@ -21,7 +45,7 @@ question_elements = [
                             id={
                                 "type": "answer-button",
                                 "index": question_id,
-                                "value": "for",
+                                "value": AnswerOptions.FOR.value,
                             },
                             color="success",
                             outline=True,
@@ -35,7 +59,7 @@ question_elements = [
                             id={
                                 "type": "answer-button",
                                 "index": question_id,
-                                "value": "against",
+                                "value": AnswerOptions.AGAINST.value,
                             },
                             color="danger",
                             outline=True,
@@ -49,7 +73,7 @@ question_elements = [
                             id={
                                 "type": "answer-button",
                                 "index": question_id,
-                                "value": "dontcare",
+                                "value": AnswerOptions.DONTCARE.value,
                             },
                             color="info",
                             outline=True,
@@ -64,7 +88,7 @@ question_elements = [
         ],
         id=f"question_id_{question_id}",
     )
-    for question_id in range(1, 11)
+    for question_id, question in enumerate(question_contents)
 ]
 
 app.layout = dbc.Container(
@@ -87,6 +111,13 @@ app.layout = dbc.Container(
             ),
             justify="center",
         ),
+        dbc.Row(
+            dbc.Col(
+                html.Div(id="output-container"),
+                width="auto",
+            ),
+            justify="center",
+        ),
     ],
     fluid=True,
     className="text-center",
@@ -96,12 +127,25 @@ app.layout = dbc.Container(
 
 @app.callback(
     [
-        Output({"type": "answer-button", "index": MATCH, "value": "for"}, "outline"),
         Output(
-            {"type": "answer-button", "index": MATCH, "value": "against"}, "outline"
+            {"type": "answer-button", "index": MATCH, "value": "for"},
+            "outline",
         ),
         Output(
-            {"type": "answer-button", "index": MATCH, "value": "dontcare"}, "outline"
+            {
+                "type": "answer-button",
+                "index": MATCH,
+                "value": AnswerOptions.AGAINST.value,
+            },
+            "outline",
+        ),
+        Output(
+            {
+                "type": "answer-button",
+                "index": MATCH,
+                "value": AnswerOptions.DONTCARE.value,
+            },
+            "outline",
         ),
     ],
     [Input({"type": "answer-button", "index": MATCH, "value": ALL}, "n_clicks")],
@@ -116,10 +160,54 @@ def update_button_outline(*args):
 
     # Determine which buttons to outline
     return [
-        clicked_button_value != "for",
-        clicked_button_value != "against",
-        clicked_button_value != "dontcare",
+        clicked_button_value != AnswerOptions.FOR.value,
+        clicked_button_value != AnswerOptions.AGAINST.value,
+        clicked_button_value != AnswerOptions.DONTCARE.value,
     ]
+
+
+def get_selected_option_per_question(array_of_options_per_question):
+    assert len(array_of_options_per_question) == 3
+
+    if array_of_options_per_question[0] == False:
+        return AnswerOptions.FOR.value
+    if array_of_options_per_question[1] == False:
+        return AnswerOptions.AGAINST.value
+    if array_of_options_per_question[2] == False:
+        return AnswerOptions.DONTCARE.value
+    return None
+
+
+@app.callback(
+    Output("output-container", "children"),
+    [Input("submit-button", "n_clicks")],
+    [State({"type": "answer-button", "index": ALL, "value": ALL}, "outline")],
+    prevent_initial_call=True,
+)
+def update_output(n_clicks, button_outlines):
+    if n_clicks is None:
+        return "Pasirinkite, kaip būtumėte balsavę jūs ir tada pamatysite rezultatus"
+
+    # Group the button outlines into triplets for each question
+    grouped_button_outlines = [
+        button_outlines[i : i + 3] for i in range(0, len(button_outlines), 3)
+    ]
+
+    for question_id, array_of_options_per_question in enumerate(
+        grouped_button_outlines
+    ):
+        selected_option_per_question = get_selected_option_per_question(
+            array_of_options_per_question
+        )
+        if selected_option_per_question is None:
+            return f"Dar nepasirinkote atsakymo į {question_id+1} klausimą"
+
+        question_contents[question_id][
+            "user_selected_answer"
+        ] = selected_option_per_question
+
+    # TODO - pick most similar MP
+    return f"{question_contents}"
 
 
 # Run the app
